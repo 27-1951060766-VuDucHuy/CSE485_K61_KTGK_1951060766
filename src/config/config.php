d51ce399a5bc9f645c6a98fbec249560c7eee2d3
VuHuy0603 <94338951+27-1951060766-VuDucHuy@users.noreply.github.com>
2022-01-09 11:38:41 +0700

    Update config.php

--- src/config/config.php
+++ src/config/config.php
@@ -1,12 +1,10 @@
 <?php
-//B1, kết nối với quản trị CSDL
 $conn = mysqli_connect("localhost","root","", "Library");
 if (!$conn){
     die("Kết nối thất bại");
 }
 
 
-//B2: thông báo ngôn ngữ sử dụng trong CSDL cho PHP
 
 mysqli_query($conn , "SET NAME 'utf8'");
 
